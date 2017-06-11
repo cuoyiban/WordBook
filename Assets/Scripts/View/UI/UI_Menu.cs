e71bf60a43a58347191149c74560133902417bf5
shejunwei <453350856@qq.com>
2017-06-12 00:49:14 +0800

    增加了几个UI Prefab，增加了Menu脚本

---
+++ Assets/Scripts/View/UI/UI_Menu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+public class UI_Menu : MonoBehaviour {
+	public Dropdown menuDropDown;
+	public Text labelTitle;
+
+	List<System.Action> m_listAction = new List<System.Action>();
+	// Use this for initialization
+	void Start () {
+		Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Init()
+	{
+		menuDropDown.ClearOptions();
+		menuDropDown.onValueChanged.AddListener(OnChangeValue);
+		menuDropDown.value = 0;
+	}
+
+	public void OnChangeValue(int iMenuIndex)
+	{
+		m_listAction[iMenuIndex]();
+	}
+
+	public void AddItemCallBack(params object[] objects)
+	{
+		for (int i = 0; i < objects.Length / 2; i += 2)
+		{
+			string strItemName = objects[i] as string;
+			System.Action action = objects[i + 1] as System.Action;
+			m_listAction.Add(action);
+			Dropdown.OptionData option = new Dropdown.OptionData();
+			option.text = strItemName;
+			menuDropDown.options.Add(option);
+		}
+		menuDropDown.captionText.text = menuDropDown.options[0].text;
+		menuDropDown.value = 0;
+	}
+}
